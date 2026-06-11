adde9b0409e98a73bb3dcf20bbc86cb36e0e781f
Adam Bergin <arbergin@gmail.com>
2026-06-10 22:29:34 -0500

    Add shared activity dropdown options

---
+++ lib/activityOptions.ts
@@ -0,0 +1,22 @@
+export const fieldLocationOptions = [
+  "First Third",
+  "Middle Third",
+  "Final Third",
+] as const;
+
+export const gamePhaseOptions = [
+  "Attacking",
+  "Attacking to Defending Transition",
+  "Defending",
+  "Defending to Attacking Transition",
+  "Set Pieces",
+] as const;
+
+export const categoryOptions = [
+  "Ball Mastery",
+  "Game-Realistic Positional Activity",
+  "Passing Activation",
+  "Rondo",
+  "Shooting",
+  "Small-Sided Games",
+] as const;
